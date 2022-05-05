b6c9e9b9e34e2dcaca6dbfbc42434493257da884
MarufSourav <maruf.sourav99@gmail.com>
2022-05-05 03:37:43 -0400

    Added Movement

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class MouseLook : MonoBehaviour
 {
+    [Flags]
+    public enum RotationDirection 
+    {
+        None,
+        Horizontal = (1 << 0),
+        Vertical = (1 << 1)
+    }
     [SerializeField] private Vector2 sensitivity;
     [SerializeField] private float maxVerticalAngleFromHorizon;
+    [SerializeField] private RotationDirection rotationDirections;
     private Vector2 rotation;
+    
     private Vector2 GetInput() 
     {
         Vector2 input = new Vector2(
@@ -22,6 +32,14 @@ public class MouseLook : MonoBehaviour
     private void Update()
     {
         Vector2 wantedVelocity = GetInput() * sensitivity;
+        if ((rotationDirections & RotationDirection.Horizontal) == 0) 
+        {
+            wantedVelocity.x = 0;
+        }
+        if ((rotationDirections & RotationDirection.Vertical) == 0) 
+        {
+            wantedVelocity.y = 0;
+        }
         rotation += wantedVelocity * Time.deltaTime;
         rotation.y = ClampVerticalAngle(rotation.y);
         transform.localEulerAngles = new Vector3(rotation.y, rotation.x, 0);
