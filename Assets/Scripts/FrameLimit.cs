5725438d24d1549b1dc5b36509659d7d704ddfc6
MarufSourav <maruf.sourav99@gmail.com>
2022-05-07 00:59:13 -0400

    FrameLimit Script Fix

--- Assets/Scripts/FrameLimit.cs
+++ Assets/Scripts/FrameLimit.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Framelimit : MonoBehaviour
+public class FrameLimit : MonoBehaviour
 {
     // Start is called before the first frame update
     private void Awake()
