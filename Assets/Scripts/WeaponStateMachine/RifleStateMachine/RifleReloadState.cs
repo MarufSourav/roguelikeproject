308a150729d4bc25fd946ad05a70969b44b9e0cf
MarufSourav <maruf.sourav99@gmail.com>
2022-05-28 01:44:18 -0400

    ReAdded Rifle Reload Sound

--- Assets/Scripts/WeaponStateMachine/RifleStateMachine/RifleReloadState.cs
+++ Assets/Scripts/WeaponStateMachine/RifleStateMachine/RifleReloadState.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using UnityEngine;
 public class RifleReloadState : RifleBaseState
 {
+    bool alreadyPlayedP1;
+    bool alreadyPlayedP2;
     public override void EnterState(RifleStateManager rifle){
+        alreadyPlayedP1 = false;
+        alreadyPlayedP2 = false;
         rifle.crosshair.SetActive(true);
         rifle.cameraResetTime = 15f;
         rifle.rifleGunShotAmmount = 0;
@@ -20,19 +24,29 @@ public class RifleReloadState : RifleBaseState
         //Rifle Reload Position
         rifle.RifleReload.transform.localPosition = Vector3.Lerp(rifle.RifleReload.transform.localPosition, new Vector3(0.06f, 0f, 0f), (12f / rifle.ps.reloadTime) * Time.deltaTime);
         rifle.RifleReload.transform.localRotation = Quaternion.Slerp(rifle.RifleReload.transform.localRotation, Quaternion.Euler(0f, 0f, -35f), (12f / rifle.ps.reloadTime) * Time.deltaTime);
-        if (rifle.reloadTime > 0.3f && rifle.reloadTime < rifle.ps.reloadTime * 0.75f)
-            //Magazine Position
-            rifle.RifleMag.transform.localPosition = Vector3.LerpUnclamped(rifle.RifleMag.transform.localPosition, new Vector3(0, -0.2f, -0.045f), (12f / rifle.ps.reloadTime) * Time.deltaTime);
         //Magazine Position
+        if (rifle.reloadTime > 0.3f && rifle.reloadTime < rifle.ps.reloadTime * 0.75f)
+        {
+            if (!alreadyPlayedP2) {
+                rifle.audio.Play("RifleReloadP2");
+                alreadyPlayedP2 = true;
+            }                
+            rifle.RifleMag.transform.localPosition = Vector3.LerpUnclamped(rifle.RifleMag.transform.localPosition, new Vector3(0, -0.2f, -0.045f), (12f / rifle.ps.reloadTime) * Time.deltaTime);
+        }
         else
             rifle.RifleMag.transform.localPosition = Vector3.LerpUnclamped(rifle.RifleMag.transform.localPosition, new Vector3(0, 0f, -0.045f), (12f / rifle.ps.reloadTime) * Time.deltaTime);
 
+        if (rifle.reloadTime >= rifle.ps.reloadTime * 0.85f && !alreadyPlayedP1)
+        {
+            rifle.audio.Play("RifleReloadP1");
+            alreadyPlayedP1 = true;
+        }
         if (rifle.reloadTime >= rifle.ps.reloadTime)
         {
             rifle.ps.magAmmo = rifle.ps.maxAmmo;
-            rifle.reloadTime = 0f;
-            rifle.SwitchState(rifle.rifleActive);
+            rifle.reloadTime = 0f;            
             rifle.AmmoCounterRifle.color = Color.grey;
+            rifle.SwitchState(rifle.rifleActive);            
         }
     }
 }
